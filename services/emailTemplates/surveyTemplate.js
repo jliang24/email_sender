9d83694007dc90b8039c2d3db57fe93bcd8977f4
jliang24 <jeffliang@nevada.unr.edu>
2019-01-12 21:47:53 -0800

    make email text bigger

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -5,9 +5,9 @@ module.exports = survey => {
     <html>
       <body>
         <div style="text-align: center;">
-          <h3> I'd like your input! </h3>
-          <p> Please answer the following question: </p>
-          <p> ${survey.body} </p> 
+          <h2> I'd like your input! </h2>
+          <h3> Please answer the following question: </h3>
+          <h4> ${survey.body} </h4> 
           <div> 
             <a href="${keys.redirectDomain}/api/surveys/${
     survey.id
